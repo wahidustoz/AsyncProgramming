704cf23cec5937edbdac56c6501e47d3da57882e
david.wahid <david.wahid@gluwa.com>
2022-11-23 11:07:53 +0500

    Add project files.

---
+++ CpuBound/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+
+BigInteger GetNthFibonacci(int n)
+{
+	if (n == 1 || n == 2)
+		return 1;
+
+	return GetNthFibonacci(n - 1) + GetNthFibonacci(n - 2);
+}
+
+async Task<BigInteger> GetNthFibonacciAsync(int n)
+	=> await Task.FromResult(GetNthFibonacci(n));
+
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+
+Console.WriteLine($"Overall time {stopwatch.ElapsedMilliseconds}ms.");
---
+++ IOBound/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+async Task ThisTakes5Seconds()
+{
+    await Task.Delay(TimeSpan.FromSeconds(5));
+    Console.WriteLine("5 second task is over.");
+}
+
+async Task ThisTakes2Second()
+{
+    await Task.Delay(TimeSpan.FromSeconds(2));
+    Console.WriteLine("2 second task is over.");
+}
+
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+
+
+Console.WriteLine($"All done in {stopwatch.ElapsedMilliseconds}ms");
+
